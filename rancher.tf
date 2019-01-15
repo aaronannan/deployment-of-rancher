f2a4bf0d040750406a649bab7b8e20c12a872d37
aaronannan <42162864+aaronannan@users.noreply.github.com>
2019-01-15 11:50:49 -0800

    Update rancher.tf

--- rancher.tf
+++ rancher.tf
@@ -3,16 +3,30 @@ provider "aws" {
 access_key = ""
 secret_key = ""
 region = us-east-1
-
-tags = {
-   Name = "Rancher"
-  }
- }
+}
 
 
 resource "aws_instance" "rancher" {
 ami = "ami-06af601d319a5b99b"
 instance_type = "t2.micro"
+
+   provisioner "file" {
+    source      = "rancher.sh"
+    destination = "/tmp/rancher.sh"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /tmp/rancher.sh",
+      "/tmp/rancher.sh args",
+    ]
+  }
+   
+   
+   
+tags = {
+   Name = "Rancher"
+  }
 }
 
 provisioner "local-exec" {
